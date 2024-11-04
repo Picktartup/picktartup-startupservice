1198e39aa0008ed1bcf2ba0562568477a59decb9
na-rong <129728196+na-rong@users.noreply.github.com>
2024-11-04 16:44:29 +0900

    ✅ test:test코드 추후에 수정 예정

--- src/main/java/com/picktartup/startup/common/dto/ApiResponse.java
+++ src/main/java/com/picktartup/startup/common/dto/ApiResponse.java
@@ -1,4 +1,13 @@
 package com.picktartup.startup.common.dto;
 
-public class ApiResponse {
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ApiResponse<T> {
+    private final int status;
+    private final String message;
+    private final String errorCode; // 추가된 부분
+    private final T data;
 }
--- src/main/java/com/picktartup/startup/repository/StartupServiceRepository.java
+++ src/main/java/com/picktartup/startup/repository/StartupServiceRepository.java
@@ -1,6 +1,7 @@
 package com.picktartup.startup.repository;
 
 import com.picktartup.startup.entity.Startup;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface StartupServiceRepository extends JpaRepository<Startup, Long> {
 
+    @EntityGraph(attributePaths = {"wallet"})
     // 진행도가 높은 순으로 상위 6개 스타트업 조회
     List<Startup> findTop6ByOrderByProgressDesc();
 }
