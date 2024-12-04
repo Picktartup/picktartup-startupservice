cd0022c7cee74cc623d09c84bf1e085cd31e4af9
na-rong <129728196+na-rong@users.noreply.github.com>
2024-12-04 14:11:27 +0900

    🐛 fix : develop merge 충돌 해결

--- src/main/java/com/picktartup/startup/StartupApplication.java
+++ src/main/java/com/picktartup/startup/StartupApplication.java
@@ -2,6 +2,7 @@ package com.picktartup.startup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,10 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = "com.picktartup.startup.repository.jpa")
 @EnableElasticsearchRepositories(basePackages = "com.picktartup.startup.repository.elasticsearch")
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class StartupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StartupApplication.class, args);
 
 		SpringApplication.run(StartupApplication.class, args);
 
